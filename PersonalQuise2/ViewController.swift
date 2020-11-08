2b9b0574a8e6268ee0306746ed369d74a28e7326
Mikhail <bskfrost3@yandex.ru>
2020-11-09 00:46:12 +0700

    Initial Commit

---
+++ PersonalQuise2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuise2
+//
+//  Created by mac on 09.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
